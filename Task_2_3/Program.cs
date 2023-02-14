33717be5fee7fae4d6e6cd85478b1bf381a137e4
Alex Mark <alexvmar@yandex.ru>
2023-02-14 16:11:45 +0300

    changed2 files homework

--- Task_2_3/Program.cs
+++ Task_2_3/Program.cs
@@ -5,3 +5,23 @@
 6 -> да
 7 -> да
 1 -> нет */
+
+Console.Clear();
+
+Console.Write("Введите день недели от 1 до 7: ");
+int num = int.Parse(Console.ReadLine()!);
+if (num >= 1 && num < 6)
+{
+    Console.WriteLine($"{num} -> нет");
+}
+
+if (num > 5 && num <= 7)
+{
+    Console.WriteLine($"{num} -> да");
+}
+
+else
+{
+    Console.WriteLine("Некорректное число");
+    Console.WriteLine("Перезапустите программу и введите значение от 1 до 7");
+}
